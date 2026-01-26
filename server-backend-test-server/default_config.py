5e2ea9291aaac8898ab525d99a92b71bd1d3a1e0
聖傑 賴 <jesse970146@gmail.com>
2026-01-26 14:20:39 +0800

    modified some secrets to env and remove from default_config

--- server-backend-test-server/default_config.py
+++ server-backend-test-server/default_config.py
@@ -1,19 +1,20 @@
 from datetime import timedelta
+import os 
 
-# test for local
-# import os 
-# DEBUG = True
-# basedir = os.path.abspath(os.path.dirname(__file__))
-# SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "database", "data.sqlite")
-# SQLALCHEMY_DATABASE_URI = "postgresql://postgres:example@localhost:5432/postgres"
-
+# secrets -> non secrets -> use default config
 DEBUG = False
-# postgresql://使用者:密碼@主機:埠號/資料庫名稱 
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@192.168.23.11:30310/postgres"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 PROPAGATE_EXCEPTIONS = True
-SECRET_KEY = "change-this-key-in-the-application-config"
-JWT_SECRET_KEY = "change-this-key-to-something-different-in-the-application-config"
 JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=7)
 JWT_BLACKLIST_ENABLED = True
 JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
+
+# secrets -> use env
+SECRET_KEY = os.getenv("SECRET_KEY")
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+# test for local
+DEBUG = True
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "database", "data.sqlite")
